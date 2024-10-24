1b1e12382dc1074fb9e3b0e604ad31d1cb414bf4
강은우 <80903685+eunwoo66@users.noreply.github.com>
2024-10-24 17:10:48 +0900

    feat: 버튼 공통 컴포넌트 추가

---
+++ src/components/Button.tsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+
+type ButtonProps = {
+  children: React.ReactNode;
+  onClick: () => void;
+  variant?: 'primary' | 'secondary' | 'outline';
+  disabled?: boolean;
+};
+
+const Button: React.FC<ButtonProps> = ({
+  children,
+  onClick,
+  variant = 'primary',
+  disabled = false,
+}) => {
+  return (
+    <StyledButton onClick={onClick} variant={variant} disabled={disabled}>
+      {children}
+    </StyledButton>
+  );
+};
+
+export default Button;
+
+const StyledButton = styled.button<{ variant: string }>`
+  display: flex;
+  width: 335px;
+  height: 45px;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  flex-shrink: 0;
+  border: none;
+  border-radius: 10px;
+  text-align: center;
+  font-family: Pretendard;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 24px;
+
+  ${({ variant }) => {
+    switch (variant) {
+      case 'primary':
+        return css`
+          background-color: ${({ theme }) => theme.colors.primaryGreen};
+          color: ${({ theme }) => theme.colors.textWhite};
+          border: none;
+
+          &:active {
+            background-color: #32a68a;
+          }
+
+          &:disabled {
+            background-color: #c6cada;
+            cursor: not-allowed;
+          }
+        `;
+      case 'secondary':
+        return css`
+          background-color: #767a8c;
+          color: ${({ theme }) => theme.colors.textWhite};
+          border: none;
+
+          &:active {
+            background-color: #63687d;
+          }
+        `;
+      case 'outline':
+        return css`
+          background-color: transparent;
+          color: ${({ theme }) => theme.colors.primaryGreen};
+          border: 1px solid ${({ theme }) => theme.colors.primaryGreen};
+
+          &:active {
+            background-color: #eef9f6;
+          }
+        `;
+      default:
+        return '';
+    }
+  }}
+`;
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,8 +1,9 @@
 export const theme = {
   colors: {
-    primary: '#4CBFA4',
-    secondary: '#474D66',
+    primaryGreen: '#4CBFA4',
+    secondaryGray: '#474D66',
     textPrimary: '#474D66',
     textSecondary: '#8F95B2',
+    textWhite: '#F7F7FA',
   },
 };
