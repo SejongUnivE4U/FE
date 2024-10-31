6fee518e15f20c5a6c726a4dc19332be2c9f2373
강은우 <80903685+eunwoo66@users.noreply.github.com>
2024-10-31 21:37:36 +0900

    feat: 구강 검사 시작 페이지 ui 구현

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Home from './pages/home/Home';
 import Landing from './pages/landing/Landing';
 import Login from './pages/login/Login';
 import OralCheck from './pages/oral-check/OralCheck';
+import OralCheckType from './pages/oral-check/type/OralCheckType';
 import ReportList from './pages/report-list/ReportList';
 import Report from './pages/report/Report';
 import SignUp from './pages/signup/SignUp';
@@ -29,7 +30,10 @@ function App() {
             <Route path="/home" element={<Home />} />
 
             <Route path="/account-setting" element={<AccountSetting />} />
+
             <Route path="/oral-check" element={<OralCheck />} />
+            <Route path="/oral-check/type" element={<OralCheckType />} />
+
             <Route path="/report" element={<Report />} />
             <Route path="/report-list" element={<ReportList />} />
           </Route>
