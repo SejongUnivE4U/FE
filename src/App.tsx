acbca17f28325072767a6060536ffcd6a90588fc
강은우 <80903685+eunwoo66@users.noreply.github.com>
2024-11-07 19:50:25 +0900

    feat: 사진 선택 페이지 ui 구현

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Home from './pages/home/Home';
 import Landing from './pages/landing/Landing';
 import Login from './pages/login/Login';
 import OralCheck from './pages/oral-check/OralCheck';
+import OralCheckPhoto from './pages/oral-check/photo/OralCheckPhoto';
 import OralCheckType from './pages/oral-check/type/OralCheckType';
 import ReportList from './pages/report-list/ReportList';
 import Report from './pages/report/Report';
@@ -33,6 +34,7 @@ function App() {
 
             <Route path="/oral-check" element={<OralCheck />} />
             <Route path="/oral-check/type" element={<OralCheckType />} />
+            <Route path="/oral-check/photo" element={<OralCheckPhoto />} />
 
             <Route path="/report" element={<Report />} />
             <Route path="/report-list" element={<ReportList />} />
