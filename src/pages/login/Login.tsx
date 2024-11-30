485913a0109516e14a366ed6c3dba6603bb185be
강은우 <80903685+eunwoo66@users.noreply.github.com>
2024-11-30 23:10:41 +0900

    fix: 빌드 에러 수정

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import Cookies from 'js-cookie';
+// import Cookies from 'js-cookie';
 import styled from 'styled-components';
 import instance from '../../api/axios';
 import Button from '../../components/Button';
