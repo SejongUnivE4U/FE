60f583ea9267a1c84247b38ed420b6b958150fa9
강은우 <80903685+eunwoo66@users.noreply.github.com>
2024-12-01 14:04:16 +0900

    feat: 구강상태 메시지 세분화

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -8,15 +8,23 @@ import { fetchUserData } from '../../api/userApis';
 import Carousel from '../../components/Carousel';
 import ScoreDonut from '../../components/ScoreDonut';
 
-type StateType = 'initial' | 'overdue' | 'needsAttention' | 'healthy';
-
 const messages: Record<StateType, string> = {
   initial: '안녕하세요!\n지금부터 당신의 구강 관리를\n도와줄 티티입니다!', //검사 0회
   overdue: '구강 검사를 한지 오래됐어요!\n지금 바로 검사해보세요!', //검사한지 4주 지남
   needsAttention:
     '현재 구강 상태가 좋지 않아요!\n가까운 치과를 방문해보시는 건 어때요?', //최근 검사 결과 40점 이하
-  healthy: '훌륭해요!\n건강한 상태를 유지하고 있군요! 계속해서 관리해봅시다!', //나머지
+  healthy: '훌륭해요!\n건강한 상태를 유지하고 있군요! 계속해서 관리해봅시다!', // 80~100
+  moderate: '구강 상태가 보통이에요.\n조금 더 관심을 가져 보세요!', // 40~60
+  good: '구강 상태가 양호합니다!\n지금 상태를 유지해보세요!', // 60~80
 };
+
+type StateType =
+  | 'initial'
+  | 'overdue'
+  | 'needsAttention'
+  | 'moderate'
+  | 'good'
+  | 'healthy';
 
 const determineStatus = (score: number): string => {
   if (score <= 20) return '매우 위험';
@@ -60,6 +68,16 @@ export default function Home() {
           setCharacterState('overdue');
         } else if (userData.lastDiagnoseScore <= 40) {
           setCharacterState('needsAttention');
+        } else if (
+          userData.lastDiagnoseScore > 40 &&
+          userData.lastDiagnoseScore <= 60
+        ) {
+          setCharacterState('moderate');
+        } else if (
+          userData.lastDiagnoseScore > 60 &&
+          userData.lastDiagnoseScore <= 80
+        ) {
+          setCharacterState('good');
         } else {
           setCharacterState('healthy');
         }
