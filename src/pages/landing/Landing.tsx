c9347fbe306820da5f93512b8d4caaee25f6cb8c
강은우 <80903685+eunwoo66@users.noreply.github.com>
2024-12-06 01:06:34 +0900

    feat: 버전 정보 표시

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -21,7 +21,7 @@ export default function Landing() {
           회원가입하기
         </Button>
       </ButtonWrapper>
-      <VersionText>1.1.0</VersionText>
+      <VersionText>1.1.1</VersionText>
     </Container>
   );
 }
